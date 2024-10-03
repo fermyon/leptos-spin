ba40bb095046754a6fc8a778316163f08dd339d2
itowlson <ivan.towlson@fermyon.com>
2024-10-04 08:03:24 +1300

    Add URL helper on RequestParts (#43)
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- leptos-spin/src/request_parts.rs
+++ leptos-spin/src/request_parts.rs
@@ -29,4 +29,50 @@ impl RequestParts {
     pub fn scheme(&self) -> &Option<Scheme> {
         &self.scheme
     }
+    /// Get the full URL for the Request
+    pub fn url(&self) -> Option<&str> {
+        let Some((_, full_url_header)) = self.headers.iter().find(|(name, _)| name == "spin-full-url") else {
+            return None;
+        };
+        std::str::from_utf8(full_url_header).ok()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn if_url_header_is_string_then_url_returns_it() {
+        let parts = RequestParts {
+            method: Method::Get,
+            scheme: Some(Scheme::Https),
+            headers: vec![
+                ("spin-full-url".to_string(), "https://example.com/test?query".as_bytes().to_vec()),
+            ],
+        };
+        assert_eq!("https://example.com/test?query", parts.url().expect("should have had a URL"));
+    }
+
+    #[test]
+    fn if_url_header_is_missing_then_url_returns_none() {
+        let parts = RequestParts {
+            method: Method::Get,
+            scheme: Some(Scheme::Https),
+            headers: vec![],
+        };
+        assert_eq!(None, parts.url(), "should have NOT had a URL");
+    }
+
+    #[test]
+    fn if_url_header_is_not_utf8_then_url_returns_none() {
+        let parts = RequestParts {
+            method: Method::Get,
+            scheme: Some(Scheme::Https),
+            headers: vec![
+                ("spin-full-url".to_string(), vec![0xe2, 0x82, 0x28]),
+            ],
+        };
+        assert_eq!(None, parts.url(), "should have NOT had a URL");
+    }
 }
